f51270a939c3ffeb217f4081dc29c91bb5ab1dd7
sdevore <sdevore@gmail.com>
2012-04-09 15:30:06 -0700

    changes for returning a raw image, question on returning the CGImageRef, is this ARC code

--- framework/Source/GPUImageStillCamera.h
+++ framework/Source/GPUImageStillCamera.h
@@ -4,5 +4,5 @@
 
 // Photography controls
 - (void)capturePhotoProcessedUpToFilter:(GPUImageOutput<GPUImageInput> *)finalFilterInChain withCompletionHandler:(void (^)(UIImage *processedImage, NSError *error))block;
-
+- (void)capturePhotoRawWithCompletionHandler:(void (^)(CGImageRef rawImageRef, NSError *error))block;
 @end
--- framework/Source/GPUImageStillCamera.m
+++ framework/Source/GPUImageStillCamera.m
@@ -7,6 +7,7 @@
 
 @end
 
+
 @implementation GPUImageStillCamera
 
 #pragma mark -
@@ -42,7 +43,7 @@
 - (void)capturePhotoProcessedUpToFilter:(GPUImageOutput<GPUImageInput> *)finalFilterInChain withCompletionHandler:(void (^)(UIImage *processedImage, NSError *error))block;
 {
     [photoOutput captureStillImageAsynchronouslyFromConnection:[[photoOutput connections] objectAtIndex:0] completionHandler:^(CMSampleBufferRef imageSampleBuffer, NSError *error) {
-
+        
         [self captureOutput:photoOutput didOutputSampleBuffer:imageSampleBuffer fromConnection:[[photoOutput connections] objectAtIndex:0]];
         // Will need an alternate pathway for the iOS 4.0 support here
 
@@ -53,4 +54,42 @@
     }];
     return;
 }
+
+- (void)capturePhotoRawWithCompletionHandler:(void (^)(CGImageRef rawImageRef, NSError *error))block;
+{
+    [photoOutput captureStillImageAsynchronouslyFromConnection:[[photoOutput connections] objectAtIndex:0] completionHandler:^(CMSampleBufferRef imageSampleBuffer, NSError *error) {
+        // todo [scd] this is the starting point
+        [self captureOutput:photoOutput didOutputSampleBuffer:imageSampleBuffer fromConnection:[[photoOutput connections] objectAtIndex:0]];
+        // Will need an alternate pathway for the iOS 4.0 support here
+        // crop the image around the center 
+        // new gpuimagefilter
+        // pass in the texture
+       
+        CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(imageSampleBuffer); 
+        /*Lock the image buffer*/
+        CVPixelBufferLockBaseAddress(imageBuffer,0); 
+        /*Get information about the image*/
+        uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer); 
+        size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer); 
+        size_t width = CVPixelBufferGetWidth(imageBuffer); 
+        size_t height = CVPixelBufferGetHeight(imageBuffer); 
+        
+        /*We unlock the  image buffer*/
+        CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+        
+        /*Create a CGImageRef from the CVImageBufferRef*/
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB(); 
+        CGContextRef newContext = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace,  kCGImageAlphaPremultipliedLast); 
+        CGImageRef newImage = CGBitmapContextCreateImage(newContext); 
+        
+        /*We release some components*/
+        CGContextRelease(newContext); 
+        CGColorSpaceRelease(colorSpace);
+        // 
+        block(newImage, error);
+        
+    }];
+    return;
+}
+
 @end
